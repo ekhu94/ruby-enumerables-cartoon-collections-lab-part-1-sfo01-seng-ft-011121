051be2c74101bf7d9c84b554196f3cbe7c7f842b
Erik Huang <erikhuangdrums@gmail.com>
2021-01-04 21:03:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,3 @@
 def greet_characters(arr)
-  arr.each { |ele| puts "Hello #{ele}!" }
-end
-
-def list_dwarves(arr)
-  arr.each_with_index { |ele, i| puts "#{i + 1}. #{ele}" }
+  arr.each { |name| puts "Hello #{name}!" }
 end
